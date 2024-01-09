9b2392f63d2605e5780d2392e9badb8e51c5c9c4
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2024-01-09 17:47:47 -0500

    feat: redirect to home page after google OAuth

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -3,11 +3,14 @@
 import { SignupForm } from '@/components/signupForm';
 import { Button } from '@/components/ui/button';
 import { useAuth } from '@/context/AuthContext';
+import { useRouter } from 'next/navigation';
 export default function LoginPage() {
   const { signInWithGoogle } = useAuth();
+  const router = useRouter();
   const handleLoginWithGoogle = async () => {
     try {
       await signInWithGoogle();
+      router.push('/dashboard/home');
     } catch (error) {
       console.log(error);
     }
